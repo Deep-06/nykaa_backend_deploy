d3754701bbe0d04ddd96089ed5b264748d2b1050
Deep-06 <shikhadeep.hzp@gmail.com>
2024-02-23 23:02:45 +0530

    backend

--- db.js
+++ db.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 
 const mongoURI = process.env.mongoURL;
 
-const connection=mongoose.connect(mongoURI)
+const connection=mongoose.connect(mongoURI).then(()=>{
+    console.log('connected')
+}).catch((err)=>{
+console.log(err)
+})
 
 module.exports={
     connection
