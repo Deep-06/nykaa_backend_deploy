5171339c34bd342714042cc89dfd77a649d41837
Deepshikha <130358363+Deep-06@users.noreply.github.com>
2024-03-13 18:25:19 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,12 +3,13 @@ const mongoose=require('mongoose');
 
 //const mongoURI = process.env.mongoURL;
 
-const connection=mongoose.connect(process.env.mongoURL).then(()=>{
+const connection= () => {
+    mongoose.connect(process.env.mongoURL).then(()=>{
     console.log('connected')
 }).catch((err)=>{
 console.log(err)
 })
-
+}
 module.exports={
     connection
 }
