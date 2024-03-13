3f0df8ef5a09c8c00c4209731389b17f09db92fa
Deepshikha <130358363+Deep-06@users.noreply.github.com>
2024-03-13 18:24:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 const express = require('express');
+const dotenv=require('dotenv')
 const {connection} = require('./db');
 const {userRouter}=require('./routes/userRouter');
 const {productRouter}=require('./routes/productRouter');
 const cors = require("cors");
 
 const app = express();
-
+dotenv.config({path: "./env"});
 //middleware
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({extended: true}))
 
 app.use('/api',userRouter);
 app.use('/api',productRouter);
 
-
-app.listen(process.env.PORT, async () => {
-    try {
-        await connection
+connection();
+app.listen(process.env.PORT, () => {
+    
         console.log(`Server is running at http://localhost:${process.env.PORT}`);
-        console.log('connected to database')
-    } catch (err) {
-        console.log(err)
-    }
 
 })
